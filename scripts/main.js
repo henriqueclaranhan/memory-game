d2cb43dda89994fc9baf3fc430915e31cbaac8d9
Henrique <henrique.claranhan@outlook.com>
2021-09-03 17:51:36 -0400

    flip cards on click

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-// JS
+const cards = document.querySelectorAll(".card-box");
+
+const toggleFlip = (card) => {
+    if (!card.classList.contains("flip")) {
+        card.classList.toggle('flip');
+    }
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        toggleFlip(card);
+    }, false)
+});
