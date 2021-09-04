a9894ba9323d6b7c881b6661ebcb8848fbc46110
Henrique <henrique.claranhan@outlook.com>
2021-09-03 22:40:34 -0400

    manage attempts by script

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,56 @@
 const cards = document.querySelectorAll(".card-box");
+const remainingAttemptsSpan = document.querySelector("#attempts");
 
-const toggleFlip = (card) => {
-    if (!card.classList.contains("flip")) {
-        card.classList.toggle('flip');
+let remainingAttempts = 20;
+let temporarilyFlipped = [];
+let canFlipCards = true;
+
+const checkChosenCards = (card1, card2) => {
+    canFlipCards = false;
+
+    if (!card1.isEqualNode(card2)) {
+        setTimeout(() => {
+            toggleFlip(card1);
+            toggleFlip(card2);
+
+            canFlipCards = true;
+        }, 1185);
+    } else {
+        canFlipCards = true;
     }
 }
 
-cards.forEach(card => {
-    card.addEventListener("click", () => {
-        toggleFlip(card);
-    }, false)
-});
+const toggleFlip = (card) => {
+    card.classList.toggle('flip');
+
+}
+
+const handleAttempt = (card) => {
+    if (canFlipCards && remainingAttempts > 0) {
+        if (!card.classList.contains("flip")) {
+            toggleFlip(card);
+            temporarilyFlipped.push(card);
+        }
+
+        if (temporarilyFlipped.length == 2) {
+            checkChosenCards(temporarilyFlipped[0], temporarilyFlipped[1]);
+            temporarilyFlipped = [];
+            remainingAttemptsSpan.innerHTML = --remainingAttempts;
+        }
+    }
+}
+
+const startGame = () => {
+    remainingAttemptsSpan.innerHTML = remainingAttempts;
+
+    // Detect click on cards
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            handleAttempt(card);
+        }, false)
+    });
+}
+
+window.onload = () => {
+    startGame();
+}
